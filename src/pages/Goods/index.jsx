827e1acf8749b41bfd157dd7e91b49bfb82b13f1
13982720426 <1679575151@qq.com>
2021-05-20 22:01:46 +0800

    完成商品列表基本页面

--- src/pages/Goods/index.jsx
+++ src/pages/Goods/index.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
-import { Button, Avatar, Switch, message } from 'antd';
-import { PlusOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Image, Switch, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import { getGoods } from '@/services/goods';
 import CreateOrEdit from './components/CreateOrEdit';
 
@@ -46,30 +46,71 @@ const index = () => {
       title: '商品图',
       dataIndex: 'cover_url',
       hideInSearch: true,
-      render: (_, record) => <Avatar src={record} size={32} icon={<UserOutlined />} />,
+      render: (_, record) => (
+        <Image
+          width={64}
+          src={record.cover_url}
+          placeholder={<Image preview={false} src={record.cover_url} width={200} />}
+        />
+      ),
     },
     {
-      title: '姓名',
-      dataIndex: 'name',
+      title: '标题',
+      dataIndex: 'title',
     },
     {
-      title: '邮箱',
-      dataIndex: 'email',
-    },
-    {
-      title: '是否禁用',
-      dataIndex: 'is_locked',
+      title: '价格',
+      dataIndex: 'price',
       hideInSearch: true,
+    },
+    {
+      title: '库存',
+      dataIndex: 'stock',
+      hideInSearch: true,
+    },
+    {
+      title: '销量',
+      dataIndex: 'sales',
+      hideInSearch: true,
+    },
+
+    {
+      title: '是否上架',
+      dataIndex: 'is_on',
       render: (_, record) => (
         <Switch
-          checkedChildren="启用"
-          unCheckedChildren="禁用"
-          defaultChecked={record.is_locked === 0}
+          checkedChildren="已上架"
+          unCheckedChildren="未上架"
+          defaultChecked={record.is_on === 1}
           onChange={() => {
             heandleLockUser(record.id);
           }}
         />
       ),
+      valueType: 'radioButton',
+      valueEnum: {
+        1: { text: '已上架' },
+        0: { text: '未上架' },
+      },
+    },
+    {
+      title: '是否推荐',
+      dataIndex: 'is_recommend',
+      render: (_, record) => (
+        <Switch
+          checkedChildren="已推荐"
+          unCheckedChildren="未推荐"
+          defaultChecked={record.is_recommend === 1}
+          onChange={() => {
+            heandleLockUser(record.id);
+          }}
+        />
+      ),
+      valueType: 'radioButton',
+      valueEnum: {
+        1: { text: '已推荐' },
+        0: { text: '未推荐' },
+      },
     },
     {
       title: '创建时间',
@@ -78,6 +119,7 @@ const index = () => {
     },
     {
       title: '操作',
+      hideInSearch: true,
       render: (_, record) => <a onClick={() => isShowModal(true, record.id)}>编辑</a>,
     },
   ];
